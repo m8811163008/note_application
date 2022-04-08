c8de5ed673edc52546f3fbf0c4aa3e41c3415ef8
MiladHesami <m8811163008@gmail.com>
2022-04-08 15:46:17 +0430

    [failing] add test for note edition

--- test/widget_test/home_page_test.dart
+++ test/widget_test/home_page_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:note_application/cubit/notes_cubit.dart';
 import 'package:note_application/home_page.dart';
+import 'package:note_application/model/note.dart';
 import 'package:note_application/note_page.dart';
 
 void main() {
@@ -58,6 +59,54 @@ void main() {
       await tester.tap(find.byType(FloatingActionButton));
       await tester.pumpAndSettle();
       expect(find.byType(NotePage), findsOneWidget);
+    });
+
+    //in home page tests we can assert the list tapping action
+    testWidgets('Navigate to note page in edit mode',
+        (WidgetTester tester) async {
+      var notesCubit = NotesCubit();
+      await _pumpTestWidget(tester, notesCubit);
+      var expectedTitle = 'note title';
+      var expectedBody = 'note body';
+
+      notesCubit.createNote(expectedTitle, expectedBody);
+      await tester.pump();
+      await tester.tap(find.byType(ListTile));
+      await tester.pumpAndSettle();
+      expect(find.byType(NotePage), findsOneWidget);
+      expect(find.text(expectedTitle), findsOneWidget);
+      expect(find.text(expectedBody), findsOneWidget);
+    });
+  });
+
+  group('Note Page', () {
+    _pumpTestWidget(WidgetTester tester, NotesCubit cubit,
+            {required Note note}) =>
+        tester.pumpWidget(
+          MaterialApp(
+            home: NotePage(notesCubit: cubit, note: note),
+          ),
+        );
+
+    testWidgets('create in edit mode', (WidgetTester tester) async {
+      var note = Note(1, 'my note', 'note body');
+      await _pumpTestWidget(tester, NotesCubit(), note: note);
+      expect(find.text(note.title), findsOneWidget);
+      expect(find.text(note.body), findsOneWidget);
+    });
+
+    testWidgets('edit note', (WidgetTester tester) async {
+      var cubit = NotesCubit()..createNote('my note', 'note body');
+      await _pumpTestWidget(tester, cubit, note: cubit.state.notes.first);
+      await tester.enterText(find.byKey(const ValueKey('title')), 'hi');
+      await tester.enterText(find.byKey(const ValueKey('body')), 'there');
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      expect(cubit.state.notes, isNotEmpty);
+      var note = cubit.state.notes.first;
+      expect(note.title, 'hi');
+      expect(note.body, 'there');
+      expect(find.byType(NotePage), findsNothing);
     });
   });
 }
